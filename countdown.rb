3bc933bb82442a0428a531828080fdc35110844d
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-18 11:46:10 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+#Countdown to Midnight
+require "pry"
+def countdown(counter)
+    while counter>0
+        puts"#{counter} SECOND(S)!"
+        counter-=1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+#countdown with sleep
+def countdown_with_sleep(counter)
+    while counter>0
+        puts"#{counter} SECOND(S)!"
+        sleep(1)
+        counter-=1
+    end
+    "HAPPY NEW YEAR!"
 end
